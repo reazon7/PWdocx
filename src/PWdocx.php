4444579f5cdeaf47e74ad7b849be0184902acdb0
unknown <reazon7@gmail.com>
2018-06-29 16:43:35 +0700

    update

--- src/PWdocx.php
+++ src/PWdocx.php
@@ -45,7 +45,7 @@ class PWdocx {
 
 		header('Content-Description: File Transfer');
 		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename=' . $fileName . '.docx');
+		header('Content-Disposition: attachment; filename=' . $fileName);
 		header('Content-Transfer-Encoding: binary');
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
